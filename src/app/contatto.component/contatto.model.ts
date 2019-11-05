ed6a69b57bf97ede01c4b75446499dbb5388a155
Max993648 <massimiliano.pisoni@marconirovereto.it>
2019-11-05 15:34:43 +0100

    3 files modified

--- src/app/contatto.component/contatto.model.ts
+++ src/app/contatto.component/contatto.model.ts
@@ -1,28 +1,39 @@
-class Contatto {
+export class Contatto {
   nome: string;
   cognome: string;
   mail: string;
   cell: string;
-  nStelle: number;
+  nrStelle: number;
 
-  constructor(nome: string, cognome: string, mail: string, cell: string, nStelle: number){
-    this.nome=nome;
-    this.cognome=cognome;
-    this.mail=mail;
-    this.cell=cell;
-    this.nStelle=nStelle;
+  constructor(
+    nome: string,
+    cognome: string,
+    mail: string,
+    cell: string,
+    nrStelle: number
+  ) {
+    this.nome = nome;
+    this.cognome = cognome;
+    this.mail = mail;
+    this.cell = cell;
+    this.nrStelle = nrStelle;
   }
 
-  getNomeCompleto():string{
-    return this.nome + ' ' + this.cognome;
+  getNomeCompleto(): string{
+    return this.nome + " " + this.cognome;
+  }
 
+  getFullContatto(): string{
+    return this.getNomeCompleto() + " " + this.mail + " " + this.cell + " " + this.nrStelle;
   }
 
   isPreferito(){
-    return this.nStelle == 5;
+    return this.nrStelle == 5;
   }
 
   setPreferito(){
-    this.nStelle=5;
+    this.nrStelle=5;
   }
 }
+
+  
